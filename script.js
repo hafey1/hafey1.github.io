f6c2daf48321c72c65cd80c469bebdac8bf7a447
Robert <niggebruggere@g.cofc.edu>
2020-08-25 11:07:43 -0400

    checking JS functionality

---
+++ script.js
@@ -0,0 +1,20 @@
+//this JS is take from https://www.w3schools.com/howto/howto_js_vertical_tabs.asp
+function openEntry(evt, EntryName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(EntryName).style.display = "block";
+  evt.currentTarget.className += " active";
